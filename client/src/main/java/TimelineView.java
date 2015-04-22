51453fdb79a51555d6a2d2127fa1709c56a98332
johanroval <jr.johan.ro@gmail.com>
2015-04-22 20:31:22 +0200

    Buttons in gridpane now start at the right position. Spans the columns given by how long they are by days and fills all of the columns accordingly

--- client/src/main/java/TimelineView.java
+++ client/src/main/java/TimelineView.java
@@ -60,6 +60,7 @@ public class TimelineView {
 
         for(int i = 0; i < columnsInt; i++){
             ColumnConstraints columnConstraints = new ColumnConstraints();
+            columnConstraints.setMinWidth(50);
             columnConstraints.setPercentWidth(100.0);
             dayPane.getColumnConstraints().add(columnConstraints);
         }
@@ -68,6 +69,7 @@ public class TimelineView {
             rowConstraints.setPercentHeight(100.0);
             dayPane.getRowConstraints().add(rowConstraints);
         }
+        //dayPane.add(new Button(), 1, 1);
         return dayPane;
     }
 
@@ -77,12 +79,16 @@ public class TimelineView {
         for(int i = 0; i < sessionHandler.eventArrayList.size(); i++){
             Event event = sessionHandler.eventArrayList.get(i);
             Button rect = new Button(event.getEvent_title());
-            for(int j = i+1; j > 0; j--){
-                if(event.getEvent_start_datetime().getDayOfYear() == sessionHandler.eventArrayList.get(i).getEvent_start_datetime().getDayOfYear()){
+            for(int j = i; j > 0; j--){
+                if(event.getEvent_start_datetime().getDayOfYear() == sessionHandler.eventArrayList.get(j).getEvent_start_datetime().getDayOfYear()){
                     rowInt++;
                 }
             }
-          dayPane.add(rect, 1, rowInt);
+            rect.setMaxWidth(Double.POSITIVE_INFINITY);
+            System.out.println("Event " + i + " start date: " + (event.getEvent_stop_datetime().getDayOfYear() - event.getEvent_start_datetime().getDayOfYear()));
+            dayPane.setColumnSpan(rect, event.getEvent_stop_datetime().getDayOfYear() - event.getEvent_start_datetime().getDayOfYear());
+            dayPane.setFillWidth(rect, true);
+            dayPane.add(rect, event.getEvent_start_datetime().getDayOfYear() - sessionHandler.getActiveTimeline().getTimeline_start_datetime().getDayOfYear(), rowInt);
         }
     }
 }
