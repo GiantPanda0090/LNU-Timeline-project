dcd74938d58eec38ba78e35d2cee6ea721e7144a
Austin Ponten <ajaces@gmail.com>
2015-04-24 14:23:16 +0200

    Started a search timeline class, its a pain in the röv

---
+++ client/src/main/java/SearchTimeline.java
@@ -0,0 +1,78 @@
+/**
+ * Created by ajace_000 on 4/24/2015.
+ */
+
+
+
+
+
+    import javafx.beans.value.ChangeListener;
+    import javafx.beans.value.ObservableValue;
+    import javafx.collections.FXCollections;
+    import javafx.collections.ObservableList;
+    import javafx.scene.control.ListView;
+    import javafx.scene.control.TextField;
+
+
+
+
+    public class SearchTimeline {
+
+        ObservableList<String> entries = FXCollections.observableArrayList();
+        ListView list = new ListView();
+
+        @Override
+        public void search(TextField txt) {
+
+
+
+
+            txt.textProperty().addListener(
+                    new ChangeListener() {
+                        public void changed(ObservableValue observable,
+                                            Object oldVal, Object newVal) {
+                            handleSearchByKey2((String)oldVal, (String)newVal);
+                        }
+                    });
+
+
+        }
+
+
+
+        public void handleSearchByKey2(String oldVal, String newVal) {
+            // If the number of characters in the text box is less than last time
+            // it must be because the user pressed delete
+            if ( oldVal != null && (newVal.length() < oldVal.length()) ) {
+                // Restore the lists original set of entries
+                // and start from the beginning
+                list.setItems( entries );
+            }
+
+            // Break out all of the parts of the search text
+            // by splitting on white space
+            String[] parts = newVal.toUpperCase().split(" ");
+
+            // Filter out the entries that don't contain the entered text
+            ObservableList<String> subentries = FXCollections.observableArrayList();
+            for ( Object entry: list.getItems() ) {
+                boolean match = true;
+                String entryText = (String)entry;
+                for ( String part: parts ) {
+                    // The entry needs to contain all portions of the
+                    // search string *but* in any order
+                    if ( ! entryText.toUpperCase().contains(part) ) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if ( match ) {
+                    subentries.add(entryText);
+                }
+            }
+            list.setItems(subentries);
+        }
+    }
+
+
