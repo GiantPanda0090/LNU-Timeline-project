bc97fc58c6c720297a95d7fcb4ac466eb3447062
GiantPanda0090 <liqischool@gmail.com>
2015-04-29 13:51:43 +0200

    user class comment

--- client/src/main/java/backend/User.java
+++ client/src/main/java/backend/User.java
@@ -9,13 +9,36 @@ package backend;
 * BACKEND
 */
 
+/* METHOD INDEX */
+/* USE CTRL+F TO SEARCH METHOD IN IDE */
+/*
+* public String getUrl()
+* public void setUrl(String url)
+* public int getId()
+* public void setId(int id)
+* public String getUsername()
+* public void setUsername(String username)
+* public String getEmail()
+* public void setEmail(String email)
+* public String getFirst_name()
+* public void setFirst_name(String first_name)
+* public String getLast_name()
+* public void setLast_name(String last_name)
+* public LocalDateTime getDate_joined()
+* public void setDate_joined(String date_joined)
+* public String getLast_login()
+* public void setLast_login(String last_login)
+ */
+
 import java.time.LocalDateTime;
 
 /**
  * Created by nils on 2015-04-05.
  */
 public class User {
-
+/*
+*field
+ */
     String url;
     int id;
     String username;
@@ -25,58 +48,78 @@ public class User {
     String date_joined;
     String last_login;
 
-    // Empty constructor
+    /*
+    *constructor
+     */
     public User() {
     }
 
+
+    /* METHOD */
+    //*******************************************************************************************************************
+    //******************************************************************************************************************
+
+    // return url addresss
     public String getUrl() {
         return url;
     }
 
+    // update current url
     public void setUrl(String url) {
         this.url = url;
     }
 
+    //return id
     public int getId() {
         return id;
     }
 
+    // update current id
     public void setId(int id) {
         this.id = id;
     }
 
+    // return username
     public String getUsername() {
         return username;
     }
 
+    //update ccurrent username
     public void setUsername(String username) {
         this.username = username;
     }
 
+    // return email
     public String getEmail() {
         return email;
     }
 
+    //update current email
     public void setEmail(String email) {
         this.email = email;
     }
 
+    //return first name
     public String getFirst_name() {
         return first_name;
     }
 
+    //update current first name
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
 
+    //return last name
     public String getLast_name() {
         return last_name;
     }
 
+    //update current last name
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
+    // return
     public LocalDateTime getDate_joined() {
         String[] dateTimeValuesSplitedUp = date_joined.split("[-TZ: .]");
         return LocalDateTime.of(
@@ -89,15 +132,22 @@ public class User {
         );
     }
 
+    //update
     public void setDate_joined(String date_joined) {
         this.date_joined = date_joined;
     }
 
+    //return last login
     public String getLast_login() {
         return last_login;
     }
 
+    //update last log in
     public void setLast_login(String last_login) {
         this.last_login = last_login;
     }
 }
+
+//end of the class
+ //*******************************************************************************************************************
+//******************************************************************************************************************\
