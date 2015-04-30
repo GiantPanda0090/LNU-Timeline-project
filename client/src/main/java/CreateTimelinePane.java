0c7dc814ea55a3385d5012a0633102134f32fb37
wasanuday <wasan_alani89@yahoo.com>
2015-04-30 19:50:04 +0100

    description solved and new unified design for new and update buttons

--- client/src/main/java/CreateTimelinePane.java
+++ client/src/main/java/CreateTimelinePane.java
@@ -2,12 +2,10 @@ import backend.SessionHandler;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -30,9 +28,7 @@ public class CreateTimelinePane {
 
     public GridPane createTimelinePane(
             PopOver popOver,
-            VBox listIn,
-            double topIn,
-            SessionHandler sessionHandlerIn,
+          final  SessionHandler sessionHandlerIn,
             ObservableList<Label> timelineObservableListIn,
             ListView<Label> timelineListViewIn
             ){
@@ -42,53 +38,51 @@ public class CreateTimelinePane {
 
         final SessionHandler sessionHandler = sessionHandlerIn;
 
-        final VBox timelineList = listIn;
-
         final PopOver newTimeorEvent = popOver;
 
         final GridPane popPane = new GridPane();
-        popPane.setMinSize(300, 400);
+        popPane.setMinSize(300, 200);
         popPane.getStylesheets().add(this.getClass().getResource("popover.css").toExternalForm());
 
         // Name Label
         final TextField name = new TextField("Name your timeline");
-        name.setMaxWidth(200);
-        name.setMinHeight(50);
+        final TextField  descTextField = new TextField("Timeline description...");
+
         name.setFont(new Font("System", 20));
         name.setId("nametime");
-        popPane.setRowIndex(name, 0);
-        popPane.setColumnIndex(name, 0);
-        popPane.setMargin(name, new Insets(-300, 50, 0, 50));
+
+
+
+        // first datepicker
+        final DatePicker firstDate = new DatePicker();
+        // second datePicker
+        final DatePicker secondDate = new DatePicker();
 
         // first label
         final Label firstLbl = new Label("Start date");
         firstLbl.setId("timelineLabel");
-        firstLbl.setMinWidth(100);
-        popPane.setRowIndex(firstLbl, 0);
-        popPane.setColumnIndex(firstLbl, 0);
-        popPane.setMargin(firstLbl, new Insets(-150, 0, 0, 50));
-
-        // first datepicker
-        final DatePicker firstDate = new DatePicker();
-        firstDate.setMaxWidth(150);
-        popPane.setRowIndex(firstDate, 0);
-        popPane.setColumnIndex(firstDate, 0);
-        popPane.setMargin(firstDate, new Insets(-70, 0, 0, 50));
 
         // second label
         final Label secondLbl = new Label("End date");
-        secondLbl.setMinWidth(100);
-        popPane.setRowIndex(secondLbl, 0);
-        popPane.setColumnIndex(secondLbl, 0);
-        popPane.setMargin(secondLbl, new Insets(30, 0, 0, 50));
+        secondLbl.setId("timelineLabel");
 
-        // second datePicker
-        final DatePicker secondDate = new DatePicker();
-        secondDate.setMaxWidth(150);
-        popPane.setRowIndex(secondDate, 0);
-        popPane.setColumnIndex(secondDate, 0);
-        popPane.setMargin(secondDate, new Insets(120, 0, 0, 50));
 
+        Button okButton = new Button("Create");
+        Button cancelButton = new Button("Cancel");
+
+
+        HBox hBox = new HBox();
+
+        hBox.setSpacing(20);
+        hBox.getChildren().addAll(okButton,cancelButton);
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(20, 20, 20, 20));
+        vBox.setSpacing(20);
+        vBox.getChildren().addAll(name, descTextField,firstLbl, firstDate,secondLbl, secondDate,hBox);
+
+        popPane.getChildren().addAll(vBox);
+
+        /*
         // test OK rectangle
         final Rectangle okRect = new Rectangle(165, 100, Color.WHITE);
         popPane.setMargin(okRect, new Insets(300, 0, 0, 0));
@@ -106,19 +100,18 @@ public class CreateTimelinePane {
         Text cancelText = new Text("Cancel");
         popPane.setMargin(cancelText, new Insets(300, 0, 0, 220));
         cancelText.setId("canceltext");
+*/
 
 
-        popPane.getChildren().addAll(firstDate, secondDate, firstLbl, secondLbl, name, okRect, cancelRect, okText, cancelText);
-
-        okRect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        okButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             public void handle(MouseEvent event) {
                 newTimeorEvent.hide();
                 Label timelineLabel = new Label(name.getText().toString());
                 timelineLabel.setId("timelineLabel");
                 //timelineList.getChildren().addAll(timelineLabel);
-
-                sessionHandler.createTimeline(name.getText().toString(), "description", firstDate.getValue().atStartOfDay(), secondDate.getValue().atTime(23, 59));
+                sessionHandler.createTimeline(name.getText().toString(), descTextField.getText(), firstDate.getValue().atStartOfDay(), secondDate.getValue().atTime(23, 59));
+              //  sessionHandler.createTimeline(name.getText().toString(), "description", firstDate.getValue().atStartOfDay(), secondDate.getValue().atTime(23, 59));
                 GenerateTimelineList generateTimelineList = new GenerateTimelineList();
                 generateTimelineList.generateTimelineList(sessionHandler, timelineObservableList, timelineListView);
                 name.setText("Name your Timeline");
@@ -128,9 +121,11 @@ public class CreateTimelinePane {
             }
         });
 
-        cancelRect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        cancelButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             public void handle(MouseEvent event) {
+                name.setText("Name your timeline");
+                descTextField.setText("timeline description...");
                 newTimeorEvent.hide();
             }
         });
